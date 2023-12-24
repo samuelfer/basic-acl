6fc54594b353857e94609b724c373b9fa9845560
Samuel <samuelfesant@gmail.com>
2023-12-23 21:52:19 -0300

    Add middleware of control access

---
+++ app/Http/Middleware/AccessControlMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AccessControlMiddleware
+{
+    use AuthorizesRequests;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $routeName = $request->route()->getName();
+        // $ignorePermissions = config('accesslistexceptions')['ignore.permissions'];
+
+        // if (!in_array($routeName, $ignorePermissions)) {
+        //     $this->authorize($routeName);
+        // }
+       
+        $this->authorize($routeName);
+
+        return $next($request);
+    }
+}
