17eff80cfe7694f99af966e055c026e3eabd2be0
Samuel <samuelfesant@gmail.com>
2023-12-24 19:02:21 -0300

    Install plugins

--- config/adminlte.php
+++ config/adminlte.php
@@ -401,17 +401,32 @@ return [
             ],
         ],
         'Select2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
+                    'asset' => true,
+                    'location' => '../vendor/select2/js/select2.min.js',
                 ],
                 [
                     'type' => 'css',
                     'asset' => false,
-                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
+                    'location' => '../vendor/select2/css/select2.css',
+                ],
+            ],
+        ],
+        'bootstrapDualListbox' => [
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => '../vendor/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '../vendor/bootstrap4-duallistbox/bootstrap-duallistbox.css',
                 ],
             ],
         ],
@@ -426,12 +441,12 @@ return [
             ],
         ],
         'Sweetalert2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8',
+                    'asset' => true,
+                    'location' => 'vendor/sweetalert2/sweetalert2.all.min.js',
                 ],
             ],
         ],
