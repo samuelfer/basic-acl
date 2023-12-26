9831cc2dc1992250b607dec77affc881c1e669b7
Samuel <samuelfesant@gmail.com>
2023-12-26 19:06:46 -0300

    Add messages failed login

---
+++ lang/vendor/adminlte/en/auth.php
@@ -0,0 +1,5 @@
+<?php
+    return [
+        'failed' => 'Login ou senha incorretos',
+        'throttle' => 'Muitas tentativas. Por favor tente novamente em :seconds segundos.',
+    ];
---
+++ lang/vendor/adminlte/pt-br/auth.php
@@ -0,0 +1,6 @@
+<?php
+    return [
+        'validation.required' => 'O email é obrigatório',
+        'failed' => 'Login ou senha incorretos',
+        'throttle' => 'Samuel Too many login attempts. Please try again in :seconds seconds.',
+    ];
