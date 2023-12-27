e2bda92220f14f062ebc1b95bf089388650e72ae
Samuel <samuelfesant@gmail.com>
2023-12-26 23:14:34 -0300

    Remove import

--- app/Http/Controllers/Admin/AlterPasswordController.php
+++ app/Http/Controllers/Admin/AlterPasswordController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdatePassword;
 use App\Models\User;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
